b138cd2107562d899b5e6c139d22c25f4e62f0f3
TimothyDevSWO <timothy.brandt@softwareone.com>
2024-10-14 10:01:34 +0200

    add login component

---
+++ src/components/auth/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
+import {MatDivider} from '@angular/material/divider';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    MatButton,
+    MatCardHeader,
+    MatCardContent,
+    MatDivider
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [Validators.required]);
+
+
+}
