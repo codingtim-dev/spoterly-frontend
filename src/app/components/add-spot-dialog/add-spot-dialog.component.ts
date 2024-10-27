efbc035ff955f046d53f947e5d3383268e8f8961
codingtim-dev <tmth.brandt@gmail.com>
2024-10-27 11:03:04 +0100

    add dialog

--- src/app/components/add-spot-dialog/add-spot-dialog.component.ts
+++ src/app/components/add-spot-dialog/add-spot-dialog.component.ts
@@ -1,4 +1,6 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -6,31 +8,17 @@ import {
   MatDialogRef,
   MatDialogTitle
 } from '@angular/material/dialog';
-import {inject} from '@angular/core';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {MatInput} from '@angular/material/input';
-import {MatButton} from '@angular/material/button';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {DndDirective} from '../../directives/dnd.directive';
-import {ProgressUploadComponent} from '../progress-upload/progress-upload.component';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-add-spot-dialog',
   standalone: true,
   imports: [
-    MatDialogTitle,
-    MatDialogContent,
-    MatFormField,
-    MatDialogActions,
-    MatInput,
-    MatButton,
-    MatLabel,
     FormsModule,
-    ReactiveFormsModule,
-    DndDirective,
-    ProgressUploadComponent,
-    NgIf
+    MatButton,
+    MatDialogActions,
+    MatDialogContent,
+    MatDialogTitle,
+    ReactiveFormsModule
   ],
   templateUrl: './add-spot-dialog.component.html',
   styleUrl: './add-spot-dialog.component.scss'
@@ -50,36 +38,18 @@ export class AddSpotDialogComponent {
     this.uploadForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
       description: ['', [Validators.required]],
-      file: [null, [Validators.required]],
     });
   }
 
-  onFileChange(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      const allowedTypes = ['image/jpeg', 'image/png'];
-      if (allowedTypes.includes(file.type)) {
-        this.uploadForm.patchValue({ file: file });
-      } else {
-        alert('Only .jpg and .png files are allowed.');
-        this.uploadForm.patchValue({ file: null });
-      }
-    }
-  }
-
-  onSubmit() {
-    if (this.uploadForm.valid) {
+  onSubmit(){
+    if(this.uploadForm.valid){
       const formData = new FormData();
-      formData.append('title', this.uploadForm.get('title')?.value);
-      formData.append('description', this.uploadForm.get('description')?.value);
-      formData.append('file', this.uploadForm.get('file')?.value);
+      formData.append('title', this.data.title);
+      formData.append('description', this.data.description);
 
-      // Send form data to the backend
-      console.log('Form submitted', formData);
+      console.log(formData);
     } else {
       alert('Please fill out the form correctly.');
     }
   }
-
-
 }
--- src/app/features/map/pages/map-view/map-view.component.ts
+++ src/app/features/map/pages/map-view/map-view.component.ts
@@ -6,8 +6,10 @@ import {mockSpotList} from '../../models/mockSpotList';
 import {SpotDetailsComponent} from '../../components/spot-details/spot-details.component';
 import {FormsModule} from '@angular/forms';
 import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatDialog} from '@angular/material/dialog';
+import {AddPostDialogComponent} from '../../../../components/add-post-dialog/add-post-dialog.component';
+import {NgClass, NgIf} from '@angular/common';
 import {AddSpotDialogComponent} from '../../../../components/add-spot-dialog/add-spot-dialog.component';
 
 
@@ -30,7 +32,10 @@ const locationIcon = L.icon({
     SpotDetailsComponent,
     FormsModule,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    MatButton,
+    NgIf,
+    NgClass
   ],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.scss'
@@ -41,6 +46,8 @@ export class MapViewComponent implements AfterViewInit {
   mockSpot: Spot[] = mockSpotList;
   selectedSpot: any;
   readonly dialog = inject(MatDialog);
+
+  showActions: boolean = true;
 
 
   // async call to http get method, retrieving the spots from the database
@@ -90,12 +97,32 @@ export class MapViewComponent implements AfterViewInit {
     console.log(this.selectedSpot)
   }
 
-  openDialog() {
+  toggleActions() {
+    this.showActions = !this.showActions;
+  }
+
+  openCreateNewPostDialog() {
+    const dialogRef = this.dialog.open(AddPostDialogComponent, {
+      data: "test",
+      height: '620px',
+      width: '520px',
+    });
+
+    this.showActions = false;
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("Dialog was closed")
+    })
+  }
+
+  openCreateNewSpotDialog() {
     const dialogRef = this.dialog.open(AddSpotDialogComponent, {
       data: "test",
       height: '620px',
       width: '520px',
     });
+
+    this.showActions = false;
 
     dialogRef.afterClosed().subscribe(result => {
       console.log("Dialog was closed")
