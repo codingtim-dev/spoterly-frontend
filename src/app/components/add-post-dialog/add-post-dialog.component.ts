53eb51eb0edb15730acfd39d13c969348dd26074
codingtim-dev <tmth.brandt@gmail.com>
2024-11-26 16:59:16 +0100

    update create post dialog

--- src/app/components/add-post-dialog/add-post-dialog.component.ts
+++ src/app/components/add-post-dialog/add-post-dialog.component.ts
@@ -7,11 +7,11 @@ import {
   MatDialogTitle
 } from '@angular/material/dialog';
 import {inject} from '@angular/core';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {AsyncPipe, NgIf, NgStyle} from '@angular/common';
+import {AsyncPipe, NgForOf, NgIf, NgStyle} from '@angular/common';
 import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from '@angular/material/autocomplete';
 import {mockSpotList} from '../../features/map/models/mockSpotList';
 import Spot from '../../features/map/models/Spot';
@@ -20,6 +20,8 @@ import {ImageService} from '../../services/post/image.service';
 import {SpotService} from '../../services/spot/spot.service';
 import {AuthService} from '../../services/auth/auth.service';
 import {PostService} from '../../services/post/post.service';
+import {validationSpots} from '../add-spot-dialog/validation/validation-spots';
+import {validationPosts} from './validation/validation-posts';
 
 const ALLOWED_FILE_TYPES = [
   'image/jpeg',
@@ -45,7 +47,9 @@ const ALLOWED_FILE_TYPES = [
     MatAutocomplete,
     MatOption,
     MatAutocompleteTrigger,
-    AsyncPipe
+    AsyncPipe,
+    MatError,
+    NgForOf
   ],
   templateUrl: './add-post-dialog.component.html',
   styleUrl: './add-post-dialog.component.scss'
@@ -80,8 +84,8 @@ export class AddPostDialogComponent implements OnInit {
   constructor(private imageService: ImageService, private spotService: SpotService, private authService: AuthService, private postService: PostService, private fb: FormBuilder) {
     this.uploadForm = this.fb.group({
       selectedSpot: ['', [Validators.required]],
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      description: ['', [Validators.required]],
+      title: ['', {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)], updateOn: 'blur'}],
+      description: ['', {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(255)], updateOn: 'blur'}],
       file: [null, [Validators.required]],
     });
   }
@@ -144,4 +148,6 @@ export class AddPostDialogComponent implements OnInit {
 
 
   protected readonly mockSpotList = mockSpotList;
+  protected readonly validationSpots = validationSpots;
+  protected readonly validationPosts = validationPosts;
 }
