32e4b741fd56fdfc5b850cea63e0e818c8d7c864
codingtim-dev <tmth.brandt@gmail.com>
2024-11-26 15:56:59 +0100

    update fonts and color palette, added login and register validation

--- src/app/core/auth/pages/register/register.component.ts
+++ src/app/core/auth/pages/register/register.component.ts
@@ -9,10 +9,11 @@ import {
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { AuthService } from '../../../../services/auth/auth.service';
 import RegisterModel from '../../RegisterModel';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -27,6 +28,9 @@ import RegisterModel from '../../RegisterModel';
     MatCardContent,
     MatDivider,
     MatLabel,
+    MatError,
+    NgIf,
+    NgForOf,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
@@ -41,11 +45,12 @@ export class RegisterComponent {
     private authService: AuthService,
   ) {
     this.registerForm = this.fb.group({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      firstname: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
+      username: new FormControl('', {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)], updateOn: 'blur'} ),
+      password: new FormControl('', {validators: [Validators.required, Validators.minLength(8), Validators.maxLength(20)], updateOn: 'blur'}),
+      firstname: new FormControl('', {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)], updateOn: 'blur'}),
+      lastname: new FormControl('', {validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)], updateOn: 'blur'}),
     });
+
   }
 
   showLoginForm() {
@@ -58,15 +63,35 @@ export class RegisterComponent {
 
   onSubmit() {
     // send register data to backend for signing in
+    if(this.registerForm.valid) {
+      const cred: RegisterModel = {
+        username: this.registerForm.value.username,
+        password: this.registerForm.value.password,
+        firstName: this.registerForm.value.firstname,
+        lastName: this.registerForm.value.lastname,
+      };
 
-    const cred: RegisterModel = {
-      username: this.registerForm.value.username,
-      password: this.registerForm.value.password,
-      firstName: this.registerForm.value.firstname,
-      lastName: this.registerForm.value.lastname,
-    };
+      this.authService.register(cred);
+      console.warn(this.registerForm.value);
+    }
+  }
 
-    this.authService.register(cred);
-    console.warn(this.registerForm.value);
+  account_validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters' },
+      { type: 'validUsername', message: 'Your username has already been taken' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
+    ],
+    'name': [
+      { type: 'pattern', message: 'First name is required' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long' },
+    ]
   }
 }
