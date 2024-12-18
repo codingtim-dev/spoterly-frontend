74399fe2569a2aad8f4fc906df3ff0be02fd2d77
CuttingEdge <62205605+Sxmple-DavidSchmidt@users.noreply.github.com>
2024-12-18 20:18:30 +0100

    Update validation-spots.ts

--- src/app/components/add-spot-dialog/validation/validation-spots.ts
+++ src/app/components/add-spot-dialog/validation/validation-spots.ts
@@ -1,8 +1,8 @@
 export const validationSpots = {
   name: [
     {type: 'required', message: 'Spot name is required'},
-    {type: 'minlength', message: 'Spot name must be at least 5 characters long'},
-    {type: 'maxlength', message: 'Spot name cannot be more than 25 characters long'},
+    {type: 'minlength', message: 'Spot name must be at least 3 characters long'},
+    {type: 'maxlength', message: 'Spot name cannot be more than 35 characters long'},
   ],
   coordinate: [
     {type: 'required', message: 'Coordinate is required'},
